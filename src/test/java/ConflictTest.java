cd9946b33f1f47453081a59436a0d79c5ca38151
Artem <aholevko@maxilect.com>
2022-12-29 00:20:36 +0300

    second commit conflict

--- src/test/java/ConflictTest.java
+++ src/test/java/ConflictTest.java
@@ -1,9 +1,12 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ConflictTest {
 
     @Test
     void conflictTest() {
+        int i = 3;
+        Assertions.assertTrue(i > 2);
 
     }
 }
