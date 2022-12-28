542d4c07257bf21247208bd9842c6a7177bda33e
Artem <aholevko@maxilect.com>
2022-12-29 00:17:07 +0300

    new commit conflict

--- src/test/java/ConflictTest.java
+++ src/test/java/ConflictTest.java
@@ -1,11 +1,16 @@
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+
 
 public class ConflictTest {
 
     @Test
     void conflictTest() {
-        System.out.println("qwerty");
+
+        System.out.println("qwerty123");
+
         Assertions.assertTrue(3 > 2);
     }
 }
